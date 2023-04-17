c1f8c42df1ae5d9c5d1b3bb7e3fefe0b75cc9e89
dam-cav <54503043+dam-cav@users.noreply.github.com>
2023-04-17 21:20:22 +0200

    chore: configure and run rubocop

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +20,11 @@ module TShirtShop
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # https://github.com/rubocop/rubocop-rails#rails-configuration-tip
+    config.generators.after_generate do |files|
+      parsable_files = files.filter { |file| file.end_with?('.rb') }
+      system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
+    end
   end
 end
