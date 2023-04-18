322f2090bbd128631d8aff5b5ba7b7d040a43405
dam-cav <54503043+dam-cav@users.noreply.github.com>
2023-04-18 23:35:50 +0200

    chore: install annotate and re-run migrations

--- Gemfile
+++ Gemfile
@@ -69,6 +69,8 @@ group :development do
   gem 'rubocop'
   gem 'rubocop-rails'
 
+  gem 'annotate'
+
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
--- app/models/t_shirt.rb
+++ app/models/t_shirt.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: t_shirts
+#
+#  id         :bigint           not null, primary key
+#  brand      :string(255)
+#  color      :string(255)
+#  name       :string(255)      not null
+#  price      :integer          not null
+#  quantity   :integer          default(0), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class TShirt < ApplicationRecord
 end
